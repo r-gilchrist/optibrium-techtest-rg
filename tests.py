b4894ddfc3939a2420600c614abffc03f14255a8
Ryan Gilchrist <Ryan.Gilchrist@mottmac.com>
2022-06-09 10:20:44 +0100

    [test add] no x-api-key response codes

--- tests.py
+++ tests.py
@@ -2,27 +2,40 @@ import unittest
 import requests
 
 BASE_URL = "http://127.0.0.1:5000/"
+HEADER = {"x-api-key": "skeleton-key"}
 
 
 class GetPeopleTests(unittest.TestCase):
 
     def test_success(self):
-        response = requests.get(BASE_URL + "person")
+        response = requests.get(BASE_URL + "person", headers=HEADER)
         self.assertEqual(response.status_code, 200)
+
+    def test_notoken(self):
+        response = requests.get(BASE_URL + "person")
+        self.assertEqual(response.status_code, 401)
 
 
 class PostPersonTests(unittest.TestCase):
 
     def test_success(self):
-        response = requests.post(BASE_URL + "person", json={"name": "Ryan"})
+        response = requests.post(BASE_URL + "person", headers=HEADER , json={"name": "Ryan"})
         self.assertEqual(response.status_code, 201)
+
+    def test_notoken(self):
+        response = requests.post(BASE_URL + "person", json={"name": "Ryan"})
+        self.assertEqual(response.status_code, 401)
 
 
 class DeletePersonTests(unittest.TestCase):
 
     def test_success(self):
-        response = requests.delete(BASE_URL + "person/1")
+        response = requests.delete(BASE_URL + "person/1", headers=HEADER)
         self.assertEqual(response.status_code, 204)
+
+    def test_notoken(self):
+        response = requests.delete(BASE_URL + "person/1")
+        self.assertEqual(response.status_code, 401)
 
 
 class GetStatusTests(unittest.TestCase):
