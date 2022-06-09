852d15d7342e1afc3284402d4376da82c5023054
Ryan Gilchrist <Ryan.Gilchrist@mottmac.com>
2022-06-09 10:16:22 +0100

    [refactor] success response codes

--- app.py
+++ app.py
@@ -3,24 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
+class httpResponse:
+
+    # Success codes
+    OK_GET = 200
+    OK_POST = 201
+    OK_DELETE = 204
+    OK_STATUS = 200
+
+
 @app.route("/person", methods=["GET"])
 def get_people():
-    return {}
+    return {}, httpResponse.OK_GET
 
 
 @app.route("/person", methods=["POST"])
 def post_person():
-    return {}, 201
+    return {}, httpResponse.OK_POST
 
 
 @app.route("/person/<int:id>", methods=["DELETE"])
 def delete_person(id):
-    return {}, 204
+    return {}, httpResponse.OK_DELETE
 
 
 @app.route("/status", methods=["GET"])
 def get_status():
-    return {}
+    return {}, httpResponse.OK_STATUS
 
 
 if __name__ == "__main__":
